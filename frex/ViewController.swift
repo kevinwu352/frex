114c32268397dd5fa4e603191da3615b7f8a5cf7
kevin <kevinwu352@gmail.com>
2026-01-30 14:43:28 +0800

    init

---
+++ frex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  frex
+//
+//  Created by Kevin Wu on 1/30/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
