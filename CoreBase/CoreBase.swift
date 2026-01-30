ebc7a1749d498b63eaeded0f96530f6a7aa797fe
kevin <kevinwu352@gmail.com>
2026-01-30 14:59:52 +0800

    002

---
+++ CoreBase/CoreBase.swift
@@ -0,0 +1,9 @@
+//
+//  CoreBase.swift
+//  CoreBase
+//
+//  Created by Kevin Wu on 1/30/26.
+//
+
+import Foundation
+
---
+++ CoreBaseTests/CoreBaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoreBaseTests.swift
+//  CoreBaseTests
+//
+//  Created by Kevin Wu on 1/30/26.
+//
+
+import Testing
+@testable import CoreBase
+
+struct CoreBaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
