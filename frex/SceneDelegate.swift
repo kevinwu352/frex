2881cd4d732715c78c58cc6ef8169cd9fe25c3e4
kevin <kevinwu352@gmail.com>
2026-02-04 21:55:37 +0800

    008

--- frex/SceneDelegate.swift
+++ frex/SceneDelegate.swift
@@ -14,7 +14,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
     // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-    // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    // This delegate does not imply the connecting scene or session are new
+    // (see `application:configurationForConnectingSceneSession` instead).
     print("scene will connect to session, window:\(window == nil ? "nil" : "some")")
     guard let scene = scene as? UIWindowScene else { return }
     window = UIWindow(windowScene: scene)
