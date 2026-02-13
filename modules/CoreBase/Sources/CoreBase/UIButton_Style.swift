0817ee3f933f48d912e4110bc555d681d994ca25
kevin <kevinwu352@gmail.com>
2026-02-13 18:59:34 +0800

    034

--- modules/CoreBase/Sources/CoreBase/UIButton_Style.swift
+++ modules/CoreBase/Sources/CoreBase/UIButton_Style.swift
@@ -32,8 +32,13 @@ public class StyButton: UIButton {
     public var dis: Bool { self == .disabled }
   }
 
+  // 系统高亮
+  //   (template, color)，务必给个颜色，否则会用系统某个颜色值。（如果传 original，颜色不起作用）
+  // 自动高亮
+  //   (template, color).high()，要给颜色，用颜色算出一个高亮颜色。（不准传 original）
+  //   (original, nil  ).high()，不给颜色，用图片算出一个高亮图片。（不准传 template）
   // imageReservation
-  public struct Icon {
+  public struct Image {
     public var image: (Status) -> (UIImage?, UIColor?)
     public var spacing: ((Status) -> Double?)?
     public var alignment: ((Status) -> NSDirectionalRectEdge?)?
@@ -48,14 +53,34 @@ public class StyButton: UIButton {
       self.alignment = alignment
       self.autohigh = autohigh
     }
+    func apply(_ conf: inout Configuration?, status: Status) {
+      let img = image(status)
+      if autohigh {
+        if img.1 != nil {
+          conf?.image = img.0
+          conf?.baseForegroundColor = img.1?.turn(status.high)
+        } else {
+          conf?.image = img.0?.turn(status.high, nil)
+        }
+      } else {
+        conf?.image = img.0
+        conf?.baseForegroundColor = img.1
+      }
+      if let spacing = spacing?(status) {
+        conf?.imagePadding = spacing
+      }
+      if let alignment = alignment?(status) {
+        conf?.imagePlacement = alignment
+      }
+    }
   }
-  public var image: Icon? {
+  public var image: Image? {
     didSet { setNeedsStyle() }
   }
 
   // titleLineBreakMode
   // subtitleLineBreakMode
-  public struct Text {
+  public struct Title {
     public var str: (Status) -> String?
     public var font: (Status) -> UIFont?
     public var color: (Status) -> UIColor?
@@ -76,11 +101,48 @@ public class StyButton: UIButton {
       self.alignment = alignment
       self.autohigh = autohigh
     }
+    func apply(_ conf: inout Configuration?, status: Status) {
+      let container = AttributeContainer([
+        .font: font(status) as Any,
+        .foregroundColor: color(status)?.turn(status.high && autohigh) as Any,
+      ])
+      conf?.attributedTitle = AttributedString(str(status) ?? "", attributes: container)
+      if let spacing = spacing?(status) {
+        conf?.titlePadding = spacing
+      }
+      if let alignment = alignment?(status) {
+        conf?.titleAlignment = alignment
+      }
+    }
   }
-  public var title: Text? {
+  public var title: Title? {
     didSet { setNeedsStyle() }
   }
-  public var subtitle: Text? {
+
+  public struct Subtitle {
+    public var str: (Status) -> String?
+    public var font: (Status) -> UIFont?
+    public var color: (Status) -> UIColor?
+    public var autohigh = false
+    public init(str: @escaping (Status) -> String?,
+                font: @escaping (Status) -> UIFont?,
+                color: @escaping (Status) -> UIColor?,
+                autohigh: Bool = false
+    ) {
+      self.str = str
+      self.font = font
+      self.color = color
+      self.autohigh = autohigh
+    }
+    func apply(_ conf: inout Configuration?, status: Status) {
+      let container = AttributeContainer([
+        .font: font(status) as Any,
+        .foregroundColor: color(status)?.turn(status.high && autohigh) as Any,
+      ])
+      conf?.attributedSubtitle = AttributedString(str(status) ?? "", attributes: container)
+    }
+  }
+  public var subtitle: Subtitle? {
     didSet { setNeedsStyle() }
   }
 
@@ -89,14 +151,12 @@ public class StyButton: UIButton {
     public var color: ((Status) -> UIColor?)?
     public var image: ((Status) -> (UIImage?, UIEdgeInsets?))?
     public var cornerRadius: ((Status) -> Double?)?
-    public var cornerStyle: ((Status) -> Configuration.CornerStyle?)?
     public var borderWidth: ((Status) -> Double?)?
     public var borderColor: ((Status) -> UIColor?)?
     public var autohigh = false
     public init(color: ((Status) -> UIColor?)? = nil,
                 image: ((Status) -> (UIImage?, UIEdgeInsets?))? = nil,
                 cornerRadius: ((Status) -> Double?)? = nil,
-                cornerStyle: ((Status) -> Configuration.CornerStyle?)? = nil,
                 borderWidth: ((Status) -> Double?)? = nil,
                 borderColor: ((Status) -> UIColor?)? = nil,
                 autohigh: Bool = false
@@ -104,10 +164,27 @@ public class StyButton: UIButton {
       self.color = color
       self.image = image
       self.cornerRadius = cornerRadius
-      self.cornerStyle = cornerStyle
       self.borderWidth = borderWidth
       self.borderColor = borderColor
       self.autohigh = autohigh
+    }
+    func apply(_ conf: inout Configuration?, status: Status) {
+      // baseBackgroundColor
+      if let color = color?(status) {
+        conf?.background.backgroundColor = color.turn(status.high && autohigh)
+      }
+      if let image = image?(status) {
+        conf?.background.image = image.0?.turn(status.high && autohigh, image.1)
+      }
+      if let cornerRadius = cornerRadius?(status) {
+        conf?.background.cornerRadius = cornerRadius
+      }
+      if let borderWidth = borderWidth?(status) {
+        conf?.background.strokeWidth = borderWidth
+      }
+      if let borderColor = borderColor?(status) {
+        conf?.background.strokeColor = borderColor
+      }
     }
   }
   public var background: Background? {
@@ -120,7 +197,9 @@ public class StyButton: UIButton {
   public var contentInsets: NSDirectionalEdgeInsets? {
     didSet { setNeedsStyle() }
   }
-
+  public var cornerStyle: Configuration.CornerStyle? {
+    didSet { setNeedsStyle() }
+  }
   public var showsActivityIndicator: Bool? {
     didSet { setNeedsStyle() }
   }
@@ -130,22 +209,21 @@ public class StyButton: UIButton {
       guard let btn = $0 as? StyButton else { return }
       var conf = $0.configuration
       let status = Status($0.state)
-      if let image = btn.image {
-        let img = image.image(status)
-        if image.autohigh {
-          if img.1 != nil {
-            print("111")
-            conf?.image = img.0
-            conf?.baseForegroundColor = img.1?.turn(status.high)
-          } else {
-            print("222")
-            conf?.image = img.0?.turn(status.high, nil)
-          }
-        } else {
-          print("333")
-          conf?.image = img.0
-          conf?.baseForegroundColor = img.1
-        }
+      btn.image?.apply(&conf, status: status)
+      btn.title?.apply(&conf, status: status)
+      btn.subtitle?.apply(&conf, status: status)
+      btn.background?.apply(&conf, status: status)
+      if let size = btn.buttonSize {
+        conf?.buttonSize = size
+      }
+      if let insets = btn.contentInsets {
+        conf?.contentInsets = insets
+      }
+      if let style = btn.cornerStyle {
+        conf?.cornerStyle = style
+      }
+      if let shows = btn.showsActivityIndicator {
+        conf?.showsActivityIndicator = shows
       }
       $0.configuration = conf
     }
@@ -154,6 +232,8 @@ public class StyButton: UIButton {
     NSObject.cancelPreviousPerformRequests(withTarget: self, selector: #selector(style), object: nil)
     perform(#selector(style), with: nil, afterDelay: 0)
   }
+
+  deinit { print("sty button, deinit") }
 }
 
 extension UIColor {
@@ -182,7 +262,7 @@ extension UIImage {
     return self
   }
   fileprivate func inset(_ insets: UIEdgeInsets?) -> UIImage {
-    if let insets = insets {
+    if let insets {
       return resizableImage(withCapInsets: insets, resizingMode: .stretch)
     } else {
       return self
@@ -193,13 +273,13 @@ extension UIImage {
 extension UIControl.State {
   fileprivate var name: String {
     switch self {
-    case .normal: "normal"               // 0
-    case .highlighted: "highlighted"     // 1
-    case .disabled: "disabled"           // 2
-    case .selected: "selected"           // 4
-    case .focused: "focused"             // 8
-    case .application: "application"     // 16711680
-    case .reserved: "reserved"           // 4278190080
+    case .normal: "normal"              // 0
+    case .highlighted: "highlighted"    // 1
+    case .disabled: "disabled"          // 2
+    case .selected: "selected"          // 4
+    case .focused: "focused"            // 8
+    case .application: "application"    // 16711680
+    case .reserved: "reserved"          // 4278190080
     default: "unknown \(rawValue)"
     }
   }
